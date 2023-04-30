fd69a5c60b2df146c831852e5c14a84be83ca4d4
FawadHamidi <82702029+FawadHamidi@users.noreply.github.com>
2023-04-30 19:02:39 +0530

    fourth

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CV from "../../assets/cv.pdf"
+import { Link } from 'react-router-dom'
 
 export default function CTA() {
   return (
     <div className='cta'>
       <a href={CV} download className='btn'>Download CV</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+      <Link path='/free-courses' className='btn btn-primary'>Let's Talk</Link>
     </div>
   )
 }
