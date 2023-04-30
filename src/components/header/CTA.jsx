7b247a0f4451c0f83d55dc776447607d69c169ff
FawadHamidi <82702029+FawadHamidi@users.noreply.github.com>
2023-04-30 19:04:15 +0530

    fifth

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -7,7 +7,7 @@ export default function CTA() {
     <div className='cta'>
       <a href={CV} download className='btn'>Download CV</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-      <Link path='/free-courses' className='btn btn-primary'>Let's Talk</Link>
+      <Link to='/free-courses' className='btn btn-primary'>Let's Talk</Link>
     </div>
   )
 }
