a6d8b3717f0198872ae5775748cfa4a0b84f915c
Fawadhamidi <fawadhamidy41@gmail.com>
2025-06-21 17:43:06 +0200

    updated links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -10,9 +10,9 @@ import "./header.css"
 export default function HeaderSocials() {
   return (
     <div className='header___socials'>
-      <a href='https://www.linkedin.com/in/fawad-ahmad-hamidi-944b121ba/' target="_blank"><BsLinkedin /></a>
+      <a href='https://www.linkedin.com/in/fawad-hamidi-944b121ba/' target="_blank"><BsLinkedin /></a>
       <a href='https://github.com/FawadHamidi' target="_blank"><FaGithub /></a>
-      <a href='https://www.instagram.com/fawadhamidi___' target="_blank"><BsInstagram /></a>
+      <a href='https://www.instagram.com/fawadhamidi_' target="_blank"><BsInstagram /></a>
     </div>
   )
 }
